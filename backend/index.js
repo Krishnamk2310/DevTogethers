dc612e388d74596874c7bacbaf6f4e582aacf08a
Markandey Krishna Mishra <krishnamk2310@gmail.com>
2025-08-31 16:43:16 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -80,7 +80,7 @@ const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
-app.get("/*",(req,res)=>{
+app.get("/.*/",(req,res)=>{
     res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
 });
 
